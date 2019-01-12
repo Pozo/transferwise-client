7984f4b07b148d11b272be158c646611bcab818f
Zoltan Polgar <pozo@gmx.com>
2019-01-12 15:12:11 +0100

    Initiate Gson only once

--- src/main/kotlin/com/github/pozo/serialize/DomainDeserializer.kt
+++ src/main/kotlin/com/github/pozo/serialize/DomainDeserializer.kt
@@ -12,47 +12,50 @@ import com.github.pozo.domain.Transfer
 import com.github.pozo.domain.User
 import com.google.gson.Gson
 
+val gson = Gson()
 
 object ProfilesDeserializer : ResponseDeserializable<Array<Profile>> {
-    override fun deserialize(content: String): Array<Profile> = Gson().fromJson(content, Array<Profile>::class.java)
+    override fun deserialize(content: String): Array<Profile> = gson.fromJson(content, Array<Profile>::class.java)
 }
 
 object ProfileDeserializer : ResponseDeserializable<Profile> {
-    override fun deserialize(content: String): Profile = Gson().fromJson(content, Profile::class.java)
+    override fun deserialize(content: String): Profile = gson.fromJson(content, Profile::class.java)
 }
 
 object AccountsDeserializer : ResponseDeserializable<Array<Account>> {
-    override fun deserialize(content: String): Array<Account> = Gson().fromJson(content, Array<Account>::class.java)
+    override fun deserialize(content: String): Array<Account> = gson.fromJson(content, Array<Account>::class.java)
 }
 
 object StatementDeserializer : ResponseDeserializable<Statement> {
-    override fun deserialize(content: String): Statement = Gson().fromJson(content, Statement::class.java)
+    override fun deserialize(content: String): Statement = gson.fromJson(content, Statement::class.java)
 }
 
 object CurrencyDeserializer : ResponseDeserializable<Array<Currency>> {
-    override fun deserialize(content: String): Array<Currency> = Gson().fromJson(content, Array<Currency>::class.java)
+    override fun deserialize(content: String): Array<Currency> = gson.fromJson(content, Array<Currency>::class.java)
 }
 
 object ExchangeRateDeserializer : ResponseDeserializable<Array<ExchangeRate>> {
-    override fun deserialize(content: String): Array<ExchangeRate> = Gson().fromJson(content, Array<ExchangeRate>::class.java)
+    override fun deserialize(content: String): Array<ExchangeRate> =
+        gson.fromJson(content, Array<ExchangeRate>::class.java)
 }
 
 object AddressesDeserializer : ResponseDeserializable<Array<Address>> {
-    override fun deserialize(content: String): Array<Address> = Gson().fromJson(content, Array<Address>::class.java)
+    override fun deserialize(content: String): Array<Address> = gson.fromJson(content, Array<Address>::class.java)
 }
 
 object AddressDeserializer : ResponseDeserializable<Address> {
-    override fun deserialize(content: String): Address = Gson().fromJson(content, Address::class.java)
+    override fun deserialize(content: String): Address = gson.fromJson(content, Address::class.java)
 }
 
 object UserDeserializer : ResponseDeserializable<User> {
-    override fun deserialize(content: String): User = Gson().fromJson(content, User::class.java)
+    override fun deserialize(content: String): User = gson.fromJson(content, User::class.java)
 }
 
 object TransfersDeserializer : ResponseDeserializable<Array<Transfer>> {
-    override fun deserialize(content: String): Array<Transfer> = Gson().fromJson(content, Array<Transfer>::class.java)
+    override fun deserialize(content: String): Array<Transfer> = gson.fromJson(content, Array<Transfer>::class.java)
 }
 
 object RecipientAccountDeserializer : ResponseDeserializable<Array<RecipientAccount>> {
-    override fun deserialize(content: String): Array<RecipientAccount> = Gson().fromJson(content, Array<RecipientAccount>::class.java)
+    override fun deserialize(content: String): Array<RecipientAccount> =
+        gson.fromJson(content, Array<RecipientAccount>::class.java)
 }
